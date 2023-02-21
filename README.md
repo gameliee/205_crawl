e19e2d2e4be107841989a82b9e0804a3495cd59d
dat <datnt527@viettel.com.vn>
2023-02-21 17:16:47 +0700

    temp: should extract data in more details

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 from `itviec.com`
 
+## start project
+
 ```shell
 /home/dat/setupfiles/anaconda3/envs/py310/bin/scrapy startproject itviec
 cd itviec
@@ -9,5 +11,29 @@ cd itviec
 ```
 
 ```shell
-/home/dat/setupfiles/anaconda3/envs/py310/bin/scrapy crawl itviec
+cd itviec
+/home/dat/setupfiles/anaconda3/envs/py310/bin/scrapy crawl jobs
 ```
+
+data will then save in `.marshal` format, read it by
+
+```shell
+python read_marshal.py
+```
+
+```shell
+/home/dat/setupfiles/anaconda3/envs/py310/bin/scrapy parse --spider=jobs -c parse_single_job -d 2 'https://itviec.com/it-jobs/remote-senior-devops-engineers-aws-101-digital-0110?lab_feature=preview_jd_page'
+```
+
+## note
+
+single job parser
+
+| name | xpath | note |
+|------|-------|------|
+|job detail header |"//div[@class='job-details__header']"||
+|top reason to join us |"//div[@class='job-details__top-reason-to-join-us']"||
+|job detail overview |  "//div[@class='job-details__overview']" ||
+|second title | "//div[@class='job-details__second-title']"| may have many |
+|paragraph | "//div[@class='job-details__paragraph']"| may have many |
+| employer | "//div[@class='jd-page__employer-overview']" ||
--- itviec/itviec/spiders/jobs.py
+++ itviec/itviec/spiders/jobs.py
@@ -8,8 +8,8 @@ class JobsSpider(CrawlSpider):
     start_urls = ["http://itviec.com/it-jobs"]
 
     custom_settings={
-        'FEED_URI': "all_%(time)s.jsonl",
-        'FEED_FORMAT': 'jsonl',
+        'FEED_URI': "all_%(time)s.marshal",
+        'FEED_FORMAT': 'marshal',
         'DUPEFILTER_CLASS': RFPDupeFilter,
         'AUTOTHROTTLE_ENABLED': True
     }
@@ -24,5 +24,11 @@ class JobsSpider(CrawlSpider):
         """parse job details"""
         print(f"huhuhu im here at {response.url}")
         yield {
-            'url': response.url
+            'url': response.url,
+            'job detail header': response.xpath("//div[@class='job-details__header']").get(),
+            'top reason to join us': response.xpath("//div[@class='job-details__top-reason-to-join-us']").get(),
+            'job detail overview': response.xpath("//div[@class='job-details__overview']").get(),
+            'second titles': response.xpath("//div[@class='job-details__second-title']").get(),
+            'paragraphs': response.xpath("//div[@class='job-details__paragraph']").get(),
+            'employer': response.xpath("//div[@class='jd-page__employer-overview']").get(),
         }
